b91b806225c0d442fd164c5f5f6708a0a76042d8
ezrafollower <ezrafollower@gmail.com>
2023-03-27 15:55:06 +0800

    add a id filter

--- apriltags_ros/src/tag_pose_publisher.py
+++ apriltags_ros/src/tag_pose_publisher.py
@@ -6,10 +6,11 @@ from geometry_msgs.msg import PoseStamped
 
 def callback(data):
     for detection in data.detections:
-        tag_pose = PoseStamped()
-        tag_pose.header = detection.pose.header
-        tag_pose.pose = detection.pose.pose
-        tag_pose_publisher.publish(tag_pose)
+        if detection.id == 1:
+            tag_pose = PoseStamped()
+            tag_pose.header = detection.pose.header
+            tag_pose.pose = detection.pose.pose
+            tag_pose_publisher.publish(tag_pose)
 
 if __name__ == '__main__':
     rospy.init_node('tag_pose_publisher', anonymous=True)
