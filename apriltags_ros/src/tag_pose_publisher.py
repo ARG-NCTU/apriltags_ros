191d3bbc702d8760099dd3da28b1e720654eba73
ezrafollower <ezrafollower@gmail.com>
2023-03-27 15:06:40 +0800

    catch tag pose out

---
+++ apriltags_ros/src/tag_pose_publisher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from apriltags_ros.msg import AprilTagDetectionArray
+from geometry_msgs.msg import PoseStamped
+
+def callback(data):
+    for detection in data.detections:
+        tag_pose = PoseStamped()
+        tag_pose.header = detection.pose.header
+        tag_pose.pose = detection.pose.pose
+        tag_pose_publisher.publish(tag_pose)
+
+if __name__ == '__main__':
+    rospy.init_node('tag_pose_publisher', anonymous=True)
+    tag_pose_publisher = rospy.Publisher('/tag_pose', PoseStamped, queue_size=10)
+    rospy.Subscriber('/tag_detections', AprilTagDetectionArray, callback)
+    rospy.spin()
+
